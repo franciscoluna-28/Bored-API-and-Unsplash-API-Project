82da64ef878186be40dd803ba85d3cae7cb3e466
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-04-16 17:12:33 -0400

    env set up

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,2 +1,10 @@
+import dotenv from 'dotenv'
+
+// Setting up environment variables
+dotenv.config()
+
 // Unsplash API key
 export const unsplashAccessKey = process.env.UNSPLASH_ACCESS_KEY
+
+// Server port
+export const serverPort = process.env.SERVER_PORT
