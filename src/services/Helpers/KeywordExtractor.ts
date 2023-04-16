e0361693e036af17ef5b4f743e17f415f9b4a18f
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-04-16 17:13:57 -0400

    Improved the algorithm so it retrieves a verb too

--- src/services/Helpers/KeywordExtractor.ts
+++ src/services/Helpers/KeywordExtractor.ts
@@ -1,6 +1,7 @@
 import natural, { LancasterStemmer } from 'natural'
 import { Activity } from '../../routes/types'
 import * as stopword from 'stopword'
+
 const language = 'EN'
 const defaultCategory = 'N'
 const lexicon = new natural.Lexicon(language, defaultCategory)
@@ -8,8 +9,7 @@ const ruleSet = new natural.RuleSet('EN')
 
 export function getBestKeyword (activity: Activity): string {
   // Tokeniza la actividad y remueve palabras vacías (stopwords)
-
-  const tokenizer = new natural.WordTokenizer()
+  const tokenizer = new natural.RegexpTokenizer({ pattern: / / })
   const tokens = tokenizer.tokenize(activity.activity.toLowerCase())
   const filteredTokens = stopword.removeStopwords(tokens, stopword.en)
 
@@ -17,23 +17,28 @@ export function getBestKeyword (activity: Activity): string {
   const taggedSentence = tagger.tag(filteredTokens)
   const taggedWords = taggedSentence.taggedWords
   const nouns = taggedWords.filter(({ tag }) => tag.match(/^NN/)).map(({ token }) => token)
+  const verbs = taggedWords.filter(({ tag }) => tag.match(/^VB/)).map(({ token }) => token)
 
   console.log(nouns)
+  console.log(verbs)
+
+  // Si no hay nombres, usar verbos
+  const tokensToConsider = nouns.length > 0 ? nouns : verbs
 
   // Cuenta la frecuencia de cada token y devuelve el más común
   const frequencies: Record<string, number> = {}
   let maxFrequency = 0
   let bestToken = ''
-  for (const noun of nouns) {
-    const stemmedNoun = LancasterStemmer.stem(noun)
-    if (!(stemmedNoun in frequencies)) {
-      frequencies[stemmedNoun] = 1
+  for (const token of tokensToConsider) {
+    const stemmedToken = LancasterStemmer.stem(token)
+    if (!(stemmedToken in frequencies)) {
+      frequencies[stemmedToken] = 1
     } else {
-      frequencies[stemmedNoun]++
+      frequencies[stemmedToken]++
     }
-    if (frequencies[stemmedNoun] > maxFrequency) {
-      maxFrequency = frequencies[stemmedNoun]
-      bestToken = stemmedNoun
+    if (frequencies[stemmedToken] > maxFrequency) {
+      maxFrequency = frequencies[stemmedToken]
+      bestToken = stemmedToken
     }
   }
   return bestToken
