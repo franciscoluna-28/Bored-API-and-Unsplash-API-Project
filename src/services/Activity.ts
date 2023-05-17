658b6ddc5b05f0391d1a3915610ca4a8462712e0
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-05-16 20:46:48 -0400

    Added endpoint to get stuff by category

--- src/services/Activity.ts
+++ src/services/Activity.ts
@@ -11,3 +11,19 @@ export async function getActivity (): Promise<Activity> {
     throw error
   }
 }
+
+// Getting an activity from Bored API according to its type
+export async function getActivityByType (type: string): Promise<Activity> {
+  try {
+    const response = await axios.get<Activity>(`https://www.boredapi.com/api/activity/?type=${type}`)
+    return response.data
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+
+
+
+
