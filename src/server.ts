4238df1fa49119f9d9037b7e87574690429cf5ad
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-06-03 16:05:39 -0400

    Added CORS

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express'
 import activityImageRouter from './routes/Activity'
+import cors from 'cors';
+
 
 const app = express()
+app.use(cors());
+
 
 // Using routing
 app.use('/activity', activityImageRouter)
