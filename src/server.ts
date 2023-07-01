277c5920131e63d8468c6599864f7c69209c507f
franciscoluna-28 <franciscoluna2004@gmail.com>
2023-07-01 17:00:03 -0400

    Added body parser and users route

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,46 @@
 import express from 'express'
 import activityImageRouter from './routes/Activity'
 import cors from 'cors';
+import userRouter from "./routes/Users"
+import { connectToDb } from './db';
 
 
-const app = express()
+
+
+
+
+
+export const app = express()
+
+// Middlewares
 app.use(cors());
+app.use(express.json())
 
+
+
+/* // Configuración de Mongoose
+const mongoURI = "mongodb+srv://franciscoluna82:ZmPTK22cJZMXDEqD@hobbie-app.7whwcko.mongodb.net"; // Reemplaza con la URL de tu base de datos MongoDB
+ mongoose
+      .connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      } as ConnectOptions)
+      .then((res) => {
+        console.log(
+          'Connected to Distribution API Database - Initial Connection'
+        );
+      })
+      .catch((err) => {
+        console.log(
+          `Initial Distribution API Database connection error occured -`,
+          err
+        );
+      });
+
+ */
 
 // Using routing
 app.use('/activity', activityImageRouter)
+app.use('/users', userRouter )
 
-export default app
+
